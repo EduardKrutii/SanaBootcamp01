b04c69e995ce6572d7b2592e665c2922fdbf36f9
EduardKrutii <ipz233_keyu@student.ztu.edu.ua>
2024-04-19 20:33:25 +0200

    Name_output

---
+++ Name_output/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Name_output.Models;
+using System.Diagnostics;
+
+namespace Name_output.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ShowName(string name)
+        {
+            ViewBag.Name = name;
+            return View("ShowName");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Виконав: **Крутій Едуард**
+
+Emails: edikkrutiy@gmail.com або ipz233_keyu@student.ztu.edu.ua
